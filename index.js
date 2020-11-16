0b66862b936ef77ef7f615ec06f236f1433c9d37
Gagan Singh <gagansingh.dev@outlook.com>
2020-11-16 18:47:04 -0500

    Implemented getPosts test query

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const Post = require("./models/Post");
 const User = require("./models/User");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    test: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    test: () => "Hello from GraphQL Land!",
+    async getPosts() {
+      try {
+        const posts = await Post.find({});
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
 
